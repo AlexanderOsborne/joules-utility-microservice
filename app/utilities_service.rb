332dd8562331316fbff4f6f7634a6916d0a1534d
Aiden Mendez <aidenmendez@gmail.com>
2021-02-25 00:45:03 -0700

    Chain api calls together

--- app/utilities_service.rb
+++ app/utilities_service.rb
@@ -2,7 +2,6 @@ require 'sinatra'
 require 'faraday'
 
 class UtilitiesService 
-
   def self.conn
     @@conn ||= Faraday.new(:url => 'https://utilityapi.com') do |req|
       req.headers['Authorization'] = "Bearer #{ENV['UTILITY_API_KEY']}"
@@ -10,40 +9,75 @@ class UtilitiesService
   end
 
   def self.get_new_user
-    require 'pry'; binding.pry
-    # post_form
+    post_form
+    # this runs the method chain up to activate_meters
+    # meter uid's are returned-- pass back to backend to be saved
   end
 
-  def self.existing_user
-    # only calls a few of them
+  def self.activate_existing_user(meter_uids)
+    post_activate_meters(meter_uids)
   end
+
+  # Bills take time to generate after meter activation, so backend should call this method to poll meters (check status)
+  def self.check_status(meter_uid)
+    result = poll_meter(meter_uid)
+    if result[:status] == "updated" && result[:bill_count] > 0
+      # call get_bills (should return usage data)
+      # calculate total kwh for first bill
+      # tell backend to send "completed" mailer to user
+    elsif result[:status] == "updated" && result[:bill_count] == 0
+      # tell backend to send mailer to user to tell them their request is finished but they don't have any bills
+    elsif result[:status] == "pending"
+      # bill aren't ready yet
+    elsif result[:status] == "errored"
+      # something went wrong :/
+    end
+  end
+
+  private
   
   def self.post_form
     response = conn.post do |req|
       req.url '/api/v2/forms'
     end
+
+    json = parser(response)
+    # utility needs to be passed to post_auth too
+    post_auth(json[:uid])
   end
 
-  def self.post_auth
+  def self.post_auth(uid)
     response = conn.post do |req|
-      req.url "/api/v2/forms/#{ENV['UTILITY_UID']}/test-submit"
+      req.url "/api/v2/forms/#{ uid }/test-submit"
       req.body = {"utility": "DEMO", "scenario": "residential"}.to_json
     end
+
+    json = parser(response)
+    get_auth_and_meters(json[:referral])
   end
 
-  def self.get_auth_and_meters
+  def self.get_auth_and_meters(ref)
     response = conn.get do |req|
-      req.url "/api/v2/authorizations?referrals=#{ENV['REFERRAL']}&include=meters"
+      req.url "/api/v2/authorizations?referrals=#{ ref }&include=meters"
     end
+
+    json = parser(response)
+    post_activate_meters(json[:authorizations][0][:meters][:meters])
   end
 
-# works
-  def self.post_activate_meters
+  def self.post_activate_meters(meter_arr)
     response = conn.post do |req|
       req.url "/api/v2/meters/historical-collection"
-      req.body = {"meters": ["711267"]}.to_json 
-      #Meter Uid's must be intered as an array- this is just a hardcode
+      req.body = {"meters": meter_arr}.to_json 
+      # Hardcoded example:
+      # req.body = {"meters": ["711267"]}.to_json 
     end
+
+    json = parser(response)
+
+    # After activation, meters need to be polled to check status of bills
+    # return meter uids to get_new_user method so uids can be polled
+    meter_arr
   end
   
   def self.poll_meter
@@ -68,21 +102,7 @@ class UtilitiesService
     json = JSON.parse(response.body, symbolize_names: true)
   end
 
-  # Bills take time to generate, so backend should call this method to check status of bills
-  def self.check_status(meter_uid)
-    result = poll_meter(meter_uid)
-    if result[:status] == "updated" && result[:bill_count] > 0
-      # calls get_bills (should return usage data)
-      # calculates total kwh for first bill
-      # tells backend to send "completed" mailer to user
-    elsif result[:status] == "updated" && result[:bill_count] == 0
-      # tells backend to send mailer to user to tell them their request is finished but they don't have any bills
-    elsif result[:status] == "pending"
-      # bills aren't ready yet
-    elsif result[:status] == "errored"
-      # something went wrong :/
-    end
+  def self.parser(response)
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
-
-
