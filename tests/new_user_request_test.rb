81edee866b9fad56c6ccc0825e540eee8528658b
Aiden Mendez <aidenmendez@gmail.com>
2021-02-25 01:44:33 -0700

    Update tests to pass parameters to methods

--- tests/new_user_request_test.rb
+++ tests/new_user_request_test.rb
@@ -22,32 +22,37 @@ class NewUserRequestTest < Test::Unit::TestCase
   end
 
   def test_it_authorizes
-    data = UtilitiesService.post_auth
+    uid= "143397"
+    data = UtilitiesService.post_auth(uid)
     # assert last_response.ok?
     # assert_equal 'Hello World', last_response.body
   end
 
   def test_auth_and_meters
-    data = UtilitiesService.get_auth_and_meters
+    ref = "186474"
+    data = UtilitiesService.get_auth_and_meters(ref)
     # require 'pry'; binding.pry
     # assert last_response.ok?
     # assert_equal 'Hello World', last_response.body
   end
 
   def test_activate_meters
-    data = UtilitiesService.post_activate_meters
+    meter_arr = ["711267"]
+    data = UtilitiesService.post_activate_meters(meter_arr)
     # assert last_response.ok?
     # assert_equal 'Hello World', last_response.body
   end
   
   def test_poll_meter
-    data = UtilitiesService.poll_meter
-    # require 'pry'; binding.pry
+    meter_uid = "711267"
+    data = UtilitiesService.poll_meter(meter_uid)
+
     # assert last_response.ok?
     # assert_equal 'Hello World', last_response.body
   end
 
   def test_new_user
-    data = UtilitiesService.get_bills
+    meter_uid = "711267"
+    data = UtilitiesService.get_bills(meter_uid)
   end
 end
