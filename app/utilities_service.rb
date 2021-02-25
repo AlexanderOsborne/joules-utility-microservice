8c63e9170b4f64a5576137026c8dcc79e289cbfc
Aiden Mendez <aidenmendez@gmail.com>
2021-02-24 23:50:15 -0700

    Complete service methods and create check_status method

--- app/utilities_service.rb
+++ app/utilities_service.rb
@@ -3,59 +3,85 @@ require 'faraday'
 
 class UtilitiesService 
 
-  def initialize
-    @conn = Faraday.new(:url => 'https://utilityapi.com')
-  end
-
-  def new_user
-    # call all 7 below it
-  end
-
-  def existing_user
-    # only calls a few of them
-  end
-  
-  def post_form(auth_param)
-    response = @conn.post do |req|
-      req.url '/api/v2/forms'
-      req.headers['Authorization'] = "Bearer #{auth_param}"
+  def self.conn
+    @@conn ||= Faraday.new(:url => 'https://utilityapi.com') do |req|
+      req.headers['Authorization'] = "Bearer #{ENV['UTILITY_API_KEY']}"
     end
   end
 
-  def post_auth(auth_param)
-    response = @conn.post do |req|
+  def self.get_new_user
+    require 'pry'; binding.pry
+    # post_form
+  end
+
+  def self.existing_user
+    # only calls a few of them
+  end
+  
+  def self.post_form
+    response = conn.post do |req|
+      req.url '/api/v2/forms'
+    end
+  end
+
+  def self.post_auth
+    response = conn.post do |req|
       req.url "/api/v2/forms/#{ENV['UTILITY_UID']}/test-submit"
-      req.headers['Authorization'] = "Bearer #{auth_param}"
       req.body = {"utility": "DEMO", "scenario": "residential"}.to_json
     end
   end
 
-  def get_auth_and_meters(auth_param)
-    response = @conn.get do |req|
+  def self.get_auth_and_meters
+    response = conn.get do |req|
       req.url "/api/v2/authorizations?referrals=#{ENV['REFERRAL']}&include=meters"
-      req.headers['Authorization'] = "Bearer #{auth_param}"
     end
   end
 
-  def post_activate_meters(auth_param)
-    response = @conn.post do |req|
+# works
+  def self.post_activate_meters
+    response = conn.post do |req|
       req.url "/api/v2/meters/historical-collection"
-      req.headers['Authorization'] = "Bearer #{auth_param}"
       req.body = {"meters": ["711267"]}.to_json 
       #Meter Uid's must be intered as an array- this is just a hardcode
     end
   end
-
-  def get_poll_meter(auth_param)
-    response = @conn.get do |req|
-      req.url "/api/v2/meters/historical-collection/#{ENV['METER_UID']}"
+  
+  def self.poll_meter
+    response = conn.get do |req|
+      req.url "/api/v2/meters/#{ENV['METER_UID']}"
       #Meter Uid's is not an array here - Meter UID is hardcoded in the url above
     end
-    require 'pry'; binding.pry
+    
+    json = JSON.parse(response.body, symbolize_names: true)
+    
+    {
+      status: json[:meters][0][:status], 
+      bill_count: json[:meters][0][:bill_count]
+    }
+  end
+  
+  def self.get_bills
+    response = conn.get do |req|
+      req.url "/api/v2/bills?meters=#{ENV['METER_UID']}"
+    end
+    
+    json = JSON.parse(response.body, symbolize_names: true)
   end
 
-  def get_bills(auth_param)
-
+  # Bills take time to generate, so backend should call this method to check status of bills
+  def self.check_status(meter_uid)
+    result = poll_meter(meter_uid)
+    if result[:status] == "updated" && result[:bill_count] > 0
+      # calls get_bills (should return usage data)
+      # calculates total kwh for first bill
+      # tells backend to send "completed" mailer to user
+    elsif result[:status] == "updated" && result[:bill_count] == 0
+      # tells backend to send mailer to user to tell them their request is finished but they don't have any bills
+    elsif result[:status] == "pending"
+      # bills aren't ready yet
+    elsif result[:status] == "errored"
+      # something went wrong :/
+    end
   end
 end
 
